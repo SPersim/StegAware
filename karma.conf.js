9f8e29f249b75d11afa599bac6ef6c13decc16bf
SPersim <jespersimon@live.nl>
2023-02-01 11:20:38 +0100

    Updates

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/AngularQuizApp'),
+      dir: require('path').join(__dirname, './coverage/StegAware'),
       subdir: '.',
       reporters: [
         { type: 'html' },
